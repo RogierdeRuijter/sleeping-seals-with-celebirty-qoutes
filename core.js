84bc7ef1df7e93deacb7359ac73b25490813bf6f
Circle Ci <rogierderuijter@hotmail.com>
2021-12-23 22:15:38 +0100

    use width css property

--- core.js
+++ core.js
@@ -1,27 +1,40 @@
 let currentIndex = 0;
 
 const createNextAssetEvent = () => {
-    document.dispatchEvent(new Event('next-asset'));
-}
+  document.dispatchEvent(new Event("next-asset"));
+};
 
-document.addEventListener('click', () => {
-    if (currentIndex < lengthfileNames) {
-        createNextAssetEvent();
-    } else {
-        reset();
-    }
+document.addEventListener("click", () => {
+  if (currentIndex < lengthfileNames) {
+    createNextAssetEvent();
+  } else {
+    reset();
+  }
 });
 
+let nextSealTimeout;
 const showNextAsset = () => {
-    currentSeal.src = convertNameToAsset(currentIndex);
-    currentIndex = currentIndex + 1;   
-}
-document.addEventListener('next-asset', () => {
-    showNextAsset();
+  currentSeal.src = convertNameToAsset(currentIndex);
+  currentIndex = currentIndex + 1;
+
+  clearTimeout(nextSealTimeout);
+
+  progressBar.style.transition = "width 0s linear";
+  progressBar.style.width = "0%";
+
+  nextSealTimeout = setTimeout(() => {
+    // if (eventNotFired === true) {
+    createNextAssetEvent();
+    //   eventNotFired = false;
+    // }
+  }, 10000);
+};
+document.addEventListener("next-asset", () => {
+  showNextAsset();
 });
 
 const start = () => {
-    createNextAssetEvent();
-}
+  createNextAssetEvent();
+};
 
 start();
--- src/asset-progress-bar.js
+++ src/asset-progress-bar.js
@@ -1,35 +1,24 @@
-const progressBar = document.getElementById('progress-bar');
+const progressBar = document.getElementById("progress-bar");
 let progressBarWidth = 0;
 
-let startTimeProgressBar = null;
 let eventNotFired = true;
 
-const progressStep = (timestamp) => {
-    if (!startTimeProgressBar) {
-        startTimeProgressBar = timestamp;
-    }
-    
-    let progress = timestamp - startTimeProgressBar;
-    
-    progressBar.style.width = Math.min(progress / 100, 100) + '%';
-
-    if (progress < 10000) {
-        requestAnimationFrame(progressStep);
-    } else if (eventNotFired === true) {
-        createNextAssetEvent();
-        eventNotFired = false;
-    }
+const progressStep = () => {
+  progressBar.style.transition = "width 10s linear";
+  progressBar.style.width = "100%";
 };
 
 const startProgressBar = () => {
-    progressBarWidth = 0;
-    startTimeProgressBar = null;
-    eventNotFired = true;
+  startTimeProgressBar = null;
+  eventNotFired = true;
 
-    requestAnimationFrame(progressStep);
-}
+  console.log("startProgressBar");
 
+  progressStep();
+};
 
-document.addEventListener('start-progress-bar', () => {
-    startProgressBar();
+// document.getElementById("progress-bar").style.width = "0";
+
+document.addEventListener("start-progress-bar", () => {
+  startProgressBar();
 });
